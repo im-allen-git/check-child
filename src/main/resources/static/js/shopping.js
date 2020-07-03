c3a01bddded712701673628ff63ffff148f8ed86
Alan <jxnx888@hotmail.com>
2020-07-03 13:39:49 +0800

    3d商城

--- src/main/resources/static/js/shopping.js
+++ src/main/resources/static/js/shopping.js
@@ -33,7 +33,7 @@ function itemHTML(data){
         var printerHtml = '<div class="title row clearfix">3D打印机/配件</div>';
         printerHtml += '<div class="3dPrinter row clearfix">'
         for(var i in printer){
-            printerHtml += '<div class="col-xs-12 each_good"><div class="good_wrapper clearfix">';
+            printerHtml += '<div class="col-xs-12 col-sm-6 each_good"><div class="good_wrapper clearfix">';
             printerHtml += '<div class="col-xs-4 img_wrapper"><img src="" alt=""></div>';
             printerHtml += '<div class="col-xs-8">';
             printerHtml += '<div class="good_title">带蓝牙、Wi-Fi、USB的3D打印 机，用于教育</div>';
@@ -47,7 +47,7 @@ function itemHTML(data){
         var moduleHtml = '<div class="title row clearfix">高级模型</div>';
         moduleHtml += '<div class="module row clearfix">'
         for(var i in module){
-            moduleHtml += '<div class="col-xs-6 each_module">';
+            moduleHtml += '<div class="col-xs-6 col-sm-3 each_module">';
             moduleHtml += '<div class="each_module_wrapper">';
             moduleHtml += '<div class="img_wrapper"><img src="" alt=""></div>';
             moduleHtml += '<div class="good_title text-center">带翅膀的女孩</div>';
