ef037642ebdc48f2894986fcc0fb177dac0ac99d
Alan <jxnx888@hotmail.com>
2020-12-01 17:47:56 +0800

    同步

--- src/main/resources/static/js/3DPrinting.js
+++ src/main/resources/static/js/3DPrinting.js
@@ -1756,7 +1756,12 @@ function saveAsImage(nameStr) {
 					clearCache( plane );
 					scene.remove( plane );
 					outlinePass.selectedObjects = [];
-					camera.position.set( 83, 71, 124); //45°
+                    if(currentBuildType == 0) { ////0: 普通模式 1：minecraft
+                        camera.position.set(83, 71, 124); //45°
+                    }
+                    else if(currentBuildType == 1){
+                        camera.position.set(0, 80, 127);
+                    }
 					camera.lookAt( 0, 0, 0 );
 					directionalLight.position.set( -10, 1, -20 ).normalize();
 					//threejs Y-up, 别的事Z-up,所以到处之前要旋转
