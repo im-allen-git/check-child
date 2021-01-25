e248c653c8abeb89f7e68a1068ec051bd500bfe5
jiangxianwei <1071083166@qq.com>
2021-01-25 16:26:36 +0800

    宁鑫改动

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -3,25 +3,13 @@ var firstBuild = 0; //是否第一次访问创建模型
 var firstMyWorld = 0; //是否第一次访问我的世界
 var isAndroid =true;
 $(function () {
-    var u = navigator.userAgent, app = navigator.appVersion;
-    var isAndroid = u.indexOf('Android') > -1 || u.indexOf('Linux') > -1; //g
-    var isIOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios终端
-    if (isAndroid) {
-        isAndroid = true;
-    }
-    else{
-        isAndroid = false;
-    }
-    if (isIOS) {
-        isAndroid = false;
-    }
+
 });
 //console.log(123)
 function goPage( type ) {
     type = Number(type);
-    if(isAndroid){
-        console.log("isAndroid: " + type)
-        switch (type) {
+    log("type: " + type);
+    switch (type) {
             case 1:
                 $(".save_succ,.save_name_module_bg").hide();
                 js.changeActive( "1" );//1,我的模型 2 商城 3 模型库首页 4 创建模型 5 返回上一页
@@ -54,59 +42,7 @@ function goPage( type ) {
                 break;
 
         }
-    }
-    else{
-        console.log("is iOS: " + type)
-        switch (type) {
-            case 1:
-                $(".save_succ,.save_name_module_bg").hide();
-                // window.location.href = "ios:@1" ; //1,我的模型 2 商城 3 模型库首页 4 创建模型 5 返回上一页
-                webkit.messageHandlers.jumpPage.postMessage("1");
-                break;
-            case 2:
-                // window.location.href = "ios:@2" ; //1,我的模型 2 商城 3 模型库首页 4 创建模型 5 返回上一页
-                webkit.messageHandlers.jumpPage.postMessage("2");
-                break;
-            case 3:
-                // window.location.href = "ios:@3" ; //1,我的模型 2 商城 3 模型库首页 4 创建模型 5 返回上一页
-                webkit.messageHandlers.jumpPage.postMessage("3");
-                break;
-            case 4:
-                // window.location.href="ios:@4"
-                webkit.messageHandlers.jumpPage.postMessage("4");
-                break;
-            case 5:
-                // window.location.href = "ios:@5" ; //1,我的模型 2 商城 3 模型库首页 4 创建模型 5 返回上一页
-                webkit.messageHandlers.jumpPage.postMessage("5");
-                break;
 
-            case 6:
-                // window.location.href = "ios:@6" ; //1,我的模型 2 商城 3 模型库首页 4 创建模型 5 返回上一页 6 index页面
-                webkit.messageHandlers.jumpPage.postMessage("6");
-                break;
-            case 7:
-                // window.location.href = "ios:@7" ; //1,我的模型 2 商城 3 模型库首页 4 创建模型 5 返回上一页 6 index页面 7 3d打印机 状态页 status
-                webkit.messageHandlers.jumpPage.postMessage("7");
-                break;
-            case 8:
-                // window.location.href = "ios:@8" ; //1,我的模型 2 商城 3 模型库首页 4 创建模型 5 返回上一页
-                webkit.messageHandlers.jumpPage.postMessage("8");
-                break;
-            case 61:
-                // window.location.href = "ios:@8" ; //1,我的模型 2 商城 3 模型库首页 4 创建模型 5 返回上一页
-                webkit.messageHandlers.jumpPage.postMessage("61");
-                break;
-
-        }
-    }
-
-}
-function log(value){
-    try{
-        webkit.messageHandlers.logMessage.postMessage({key:value})
-    }catch(error){
-        console.error('The native context not exist ')
-    }
 }
 function firstCheck(access, build, myworld){//0 没有访问过  1 访问过
     firstAccess = access;
---
+++ src/main/resources/static/js/commonios.js
@@ -0,0 +1,65 @@
+var firstAccess = 0; //是否第一次访问app
+var firstBuild = 0; //是否第一次访问创建模型
+var firstMyWorld = 0; //是否第一次访问我的世界
+var isAndroid =true;
+$(function () {
+});
+//console.log(123)
+function goPage( type ) {
+    type = Number(type);
+    log("type: " + type);
+    switch (type) {
+        case 1:
+            $(".save_succ,.save_name_module_bg").hide();
+            // window.location.href = "ios:@1" ; //1,我的模型 2 商城 3 模型库首页 4 创建模型 5 返回上一页
+            webkit.messageHandlers.jumpPage.postMessage("1");
+            break;
+        case 2:
+            // window.location.href = "ios:@2" ; //1,我的模型 2 商城 3 模型库首页 4 创建模型 5 返回上一页
+            webkit.messageHandlers.jumpPage.postMessage("2");
+            break;
+        case 3:
+            // window.location.href = "ios:@3" ; //1,我的模型 2 商城 3 模型库首页 4 创建模型 5 返回上一页
+            webkit.messageHandlers.jumpPage.postMessage("3");
+            break;
+        case 4:
+            // window.location.href="ios:@4"
+            webkit.messageHandlers.jumpPage.postMessage("4");
+            break;
+        case 5:
+            // window.location.href = "ios:@5" ; //1,我的模型 2 商城 3 模型库首页 4 创建模型 5 返回上一页
+            webkit.messageHandlers.jumpPage.postMessage("5");
+            break;
+
+        case 6:
+            // window.location.href = "ios:@6" ; //1,我的模型 2 商城 3 模型库首页 4 创建模型 5 返回上一页 6 index页面
+            webkit.messageHandlers.jumpPage.postMessage("6");
+            break;
+        case 7:
+            // window.location.href = "ios:@7" ; //1,我的模型 2 商城 3 模型库首页 4 创建模型 5 返回上一页 6 index页面 7 3d打印机 状态页 status
+            webkit.messageHandlers.jumpPage.postMessage("7");
+            break;
+        case 8:
+            // window.location.href = "ios:@8" ; //1,我的模型 2 商城 3 模型库首页 4 创建模型 5 返回上一页
+            webkit.messageHandlers.jumpPage.postMessage("8");
+            break;
+        case 61:
+            // window.location.href = "ios:@8" ; //1,我的模型 2 商城 3 模型库首页 4 创建模型 5 返回上一页
+            webkit.messageHandlers.jumpPage.postMessage("61");
+            break;
+
+    }
+
+}
+function log(value){
+    try{
+        webkit.messageHandlers.logMessage.postMessage({key:value})
+    }catch(error){
+        console.error('The native context not exist ')
+    }
+}
+function firstCheck(access, build, myworld){//0 没有访问过  1 访问过
+    firstAccess = access;
+    firstBuild = build;
+    firstMyWorld = myworld;
+}
